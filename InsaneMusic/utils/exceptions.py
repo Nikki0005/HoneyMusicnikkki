20c77a9a7de828e18be82f0fdaa87e331a71cb70
Ŋʌdʌŋ Pʌʀɩŋdʌ♡ <47463180+NadaanXNitric@users.noreply.github.com>
2022-10-21 17:37:41 +0530

    HoneyMusic 2.0

---
+++ InsaneMusic/utils/exceptions.py
@@ -0,0 +1,9 @@
+# Kanged By © @always_hungry365
+# Owner Mayank
+# All rights reserved. © Alisha © Insane © Yukki
+
+
+
+class AssistantErr(Exception):
+    def __init__(self, errr: str):
+        super().__init__(errr)
